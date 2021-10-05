25ee1fc3a675f6de06011b2e51cf4e1caac9c311
DavidSabau <davidnvm3@gmail.com>
2021-10-05 17:06:54 +0300

    Initial commit

--- OCEAN/src/Main.java
+++ OCEAN/src/Main.java
@@ -1,2 +1,18 @@
+
+import java.io.Console;
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args){
+        Console console = System.console();
+        if(console!=null)
+        {
+            console.writer().println("Nu exista o consola !");
+            Scanner scanner = new Scanner(console.reader());
+            System.out.println("Alegeti o optiune");
+        }
+        else
+        {
+            System.out.println("Nu exista o consola !");
+        }
+    }
 }
